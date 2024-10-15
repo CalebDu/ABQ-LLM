ec1ae324ef101470fd010b1b127c4d84ded611a9
Caleb_Du <Caleb_Du@zju.edu.cn>
2024-10-15 12:20:29 +0000

    complete basic AqCuteKernel  definition

--- engine/mma_any/aq_cute_atom.h
+++ engine/mma_any/aq_cute_atom.h
@@ -1,12 +1,8 @@
 #pragma once
 
 #include "common/base.h"
-#include "cute/arch/mma_sm80.hpp"
-#include "cute/numeric/integral_constant.hpp"
-#include "cute/numeric/math.hpp"
 #include "cute/tensor.hpp"
-#include "cute/swizzle.hpp"
-#include "cute/swizzle_layout.hpp"
+#include <cstdint>
 
 using namespace cute;
 
@@ -30,58 +26,20 @@ template <> struct AqCuteMmaAtom<ShapeBase<16, 8, 256>> {
     using mma_atom = MMA_Atom<mma_traits>;
 };
 
-// define all SmemLayout in abq kernel
-template <typename QuantType, typename ThreadBlockShape, int kThreadBlockStage>
-struct AqCuteSmemLayout {
-    static constexpr int X_BITS = QuantType::X_BITS;
-    static constexpr int W_BITS = QuantType::W_BITS;
-    static constexpr int G2S_SwizzleB = 3; // 2^3 = 8
-    static constexpr int G2S_SwizzleM = 7; // 2^7 = 128 uint1b_t = int128_t
-    static constexpr int G2S_SwizzleS = 3; // 2^3 = 8
-    static constexpr int BLOCK_M = ThreadBlockShape::M;
-    static constexpr int BLOCK_N = ThreadBlockShape::N;
-    static constexpr int BLOCK_K = ThreadBlockShape::K;
 
-    static constexpr int MainLoop_BLOCK_M = BLOCK_M * X_BITS;
-    static constexpr int MainLoop_BLOCK_N = BLOCK_N * W_BITS;
-    static constexpr int MainLoop_BLOCK_K = BLOCK_K;
-
-    // mainloop SmemLayout
-    using SmemABLayoutAtom =
-        decltype(composition(Swizzle<G2S_SwizzleB, G2S_SwizzleM, G2S_SwizzleS>{},
-                             make_layout(make_shape(Int<8>{}, Int<MainLoop_BLOCK_K>{}),
-                                         make_stride(Int<MainLoop_BLOCK_K>{}, Int<1>{}))));
-    // [x_bit * block_m, K]
-    using SmemALayout = decltype(tile_to_shape(
-        SmemABLayoutAtom{},
-        make_shape(Int<MainLoop_BLOCK_M>{}, Int<MainLoop_BLOCK_K>{}, Int<kThreadBlockStage>{})));
-    // [w_bit * block_n, K]
-    using SmemBLayout = decltype(tile_to_shape(
-        SmemABLayoutAtom{},
-        make_shape(Int<MainLoop_BLOCK_N>{}, Int<MainLoop_BLOCK_K>{}, Int<kThreadBlockStage>{})));
-    static constexpr size_t inputSmemSize = cosize(SmemALayout{}) + cosize(SmemBLayout{});
-
-    // todo SmemCLayout
-    // [x_bit * block_m, w_bit * block_n]
-
-    // epilogue SmemLayout
-    // todo SmemEpilogStore
-    // [block_m, block_n]
-
-    static constexpr size_t SmemSize = inputSmemSize;
-};
-
-// define all copy in abq kernel
-template <typename type, typename ThreadBlockShape, int kThread> struct AqCuteCopy {
-    //  mainloop G2S load async copy
+// define copy in abq kernel
+template <typename type, typename acc_type, typename ThreadBlockShape, int kThread>
+struct AqCuteCopy {
+    // mainloop G2S load copy async
     using G2SCopyOp = SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>; // 128 uint1b_t per thread
     using G2SCopyTraits = Copy_Traits<G2SCopyOp>;
     using G2SCopyAtom = Copy_Atom<G2SCopyTraits, type>;
 
-    static constexpr int G2SCopy_thread_k = ThreadBlockShape::K / 128;
+    static constexpr int G2SCopy_thread_k =
+        ThreadBlockShape::K / 128; // 128 uint1b_t(int128_t) per thread
     static constexpr int G2SCopy_thread_m = kThread / G2SCopy_thread_k;
 
-    // copy tile: thread layout[G2SCopy_thread_m, G2SCopy_thread_k] async copy uint128_t per thread
+    // CTA block G2S load copy [P * block_m, block_k] tile from [P * m, k]
     using G2SCopyA = decltype(make_tiled_copy(
         G2SCopyAtom{},
         make_layout(make_shape(Int<G2SCopy_thread_m>{}, Int<G2SCopy_thread_k>{}),
@@ -90,7 +48,7 @@ template <typename type, typename ThreadBlockShape, int kThread> struct AqCuteCo
     using G2SCopyB = G2SCopyA;
 
     // mainloop S2R load copy
-    // copy 32 uint1b_t[fragment] per thread
+    // load copy 32 uint1b_t(int32_t) fragment per thread
     using S2RCopyOp = UniversalCopy<int32_t>;
     using S2RCopyTraits = Copy_Traits<S2RCopyOp>;
     using S2RCopyAtom = Copy_Atom<S2RCopyTraits, type>;
@@ -98,14 +56,28 @@ template <typename type, typename ThreadBlockShape, int kThread> struct AqCuteCo
     using S2RCopyAtomB = S2RCopyAtom;
 
     // mainloop R2S store copy
-    //todo
+    // store copy 2 int32_t(int64_t) fragment per thread
+    using R2SCopyAtomC = Copy_Atom<UniversalCopy<int64_t>, acc_type>;
 
-    // epilogue S2R load copy
-    //todo
 
-    // epilogue R2S store copy
-    //todo
-
-    // epilogue S2G store copy vectorization
-    //todo
+    // epilogue S2R load copy vectorization
+    static constexpr int Epilog_thread_n =
+        ThreadBlockShape::N / 4; // 4 int_32_t(uint128_t) per thread
+    static constexpr int Epilog_thread_m = kThread / G2SCopy_thread_k;
+    
+    // S2R load copy 4 int32 (128bit) per thread  
+    using EpilogS2RCopyAtom = Copy_Atom<UniversalCopy<cute::uint128_t>, acc_type>;
+    // CTA block S2R load copy [block_m, block_n] tile from [P * block_m, Q * block_n]
+    using EpilogS2RCopy =
+        decltype(make_tiled_copy(EpilogS2RCopyAtom{},
+                                 make_layout(Int<Epilog_thread_m>{}, Int<Epilog_thread_n>{}),
+                                 make_layout(make_shape(_1{}, _4{}))));
+    
+    // epilogue R2G store copy vectorization
+    using EpilogR2GCopyAtom = EpilogS2RCopyAtom;
+    // CTA block R2G store copy [block_m, block_n] tile to [m, n]
+    using EpilogR2GCopy =
+        decltype(make_tiled_copy(EpilogR2GCopyAtom{},
+                                 make_layout(Int<Epilog_thread_m>{}, Int<Epilog_thread_n>{}),
+                                 make_layout(make_shape(_1{}, _4{}))));
 };
--- engine/mma_any/aq_cute_kernel.h
+++ engine/mma_any/aq_cute_kernel.h
@@ -3,7 +3,6 @@
 #include "common/base.h"
 #include "common/memory.h"
 #include "aq_cute_atom.h"
-#include "cute/numeric/int.hpp"
 #include <cstdint>
 
 template <
@@ -11,7 +10,7 @@ template <
     typename QuantType,
     // tiling shapes
     typename ThreadBlockShape,
-    // warp shape [m, n, k] for tiled_mma, e.g. [2, 2, 1] or [1, 2, 1]
+    // warp shape [m, n, k] for building tiled_mma, e.g. [2, 2, 1] or [1, 2, 1]
     typename WarpShape,
     // mma op shape
     typename MmaShape,
@@ -28,8 +27,8 @@ struct AqCuteKernel {
     using type = cute::uint1_t;
     using acc_type = AccumulatorType;
 
-    static constexpr int X_BITS = QuantType::X_BITS;
-    static constexpr int W_BITS = QuantType::W_BITS;
+    static constexpr int X_BITS = QuantType::X_BITS; // P bit
+    static constexpr int W_BITS = QuantType::W_BITS; // Q bit
     // CTA tile shape
     static constexpr int BLOCK_M = ThreadBlockShape::M;
     static constexpr int BLOCK_N = ThreadBlockShape::N;
@@ -59,27 +58,69 @@ struct AqCuteKernel {
     static_assert(MainLoop_BLOCK_K % (MMA_K * Warp_K) == 0,
                   "BLOCK_K must be divisible by mma op k");
 
-    static constexpr bool GridMapping = GridMappingXYToMN;
-
+    // --- TiledMMA ---
     using mma_atom = typename AqCuteMmaAtom<MmaShape>::mma_atom;
 
-    // tiled CTA tile shape, [mma_m * warp_m, mma_n * warp_n, mma_k * warp_k]
+    // tiled_MMA shape, [mma_m * warp_m, mma_n * warp_n, mma_k * warp_k]
     // auto compute warp_tile shape
-    using mma = decltype(make_tiled_mma(
+    using TiledMMA = decltype(make_tiled_mma(
         mma_atom{},
         make_layout(make_shape(Int<WarpShape::M>{}, Int<WarpShape::N>{}, Int<WarpShape::K>{}))));
 
-    static constexpr int kThread = size(mma{});
+    // nums of thread in thread block
+    static constexpr int blockDims = size(TiledMMA{});
 
-    using SmemLayout = AqCuteSmemLayout<QuantType, ThreadBlockShape, kThreadBlockStage>;
+    // --- Smem Layout ---
+    // AB Swizzle
+    static constexpr int G2S_SwizzleB = 3; // 2^3 = 8
+    static constexpr int G2S_SwizzleM = 7; // 2^7 = 128 uint1b_t = int128_t
+    static constexpr int G2S_SwizzleS = 3; // 2^3 = 8
+    // SmemLayoutAtom [8, block_k]
+    using SmemABLayoutAtom =
+        decltype(composition(Swizzle<G2S_SwizzleB, G2S_SwizzleM, G2S_SwizzleS>{},
+                             make_layout(make_shape(Int<8>{}, Int<MainLoop_BLOCK_K>{}),
+                                         make_stride(Int<MainLoop_BLOCK_K>{}, Int<1>{}))));
 
-    using SmemALayout = typename SmemLayout::SmemALayout;
-    using SmemBLayout = typename SmemLayout::SmemBLayout;
+    // SmemALayout [x_bit * block_m, block_k, stage]
+    using SmemALayout = decltype(tile_to_shape(
+        SmemABLayoutAtom{},
+        make_shape(Int<MainLoop_BLOCK_M>{}, Int<MainLoop_BLOCK_K>{}, Int<kThreadBlockStage>{})));
+    // SmemBLayout [w_bit * block_n, block_k, stage]
+    using SmemBLayout = decltype(tile_to_shape(
+        SmemABLayoutAtom{},
+        make_shape(Int<MainLoop_BLOCK_N>{}, Int<MainLoop_BLOCK_K>{}, Int<kThreadBlockStage>{})));
 
-    // mainloop interface
+    // !question: inputSmemSize / 8 cause illegal memory access!
+    static constexpr size_t ASmemSize = cosize(SmemALayout{});
+    static constexpr size_t BSmemSize = cosize(SmemBLayout{});
+    static constexpr size_t inputSmemSize = ASmemSize + BSmemSize;
+
+    // todo swizzle SmemCLayout
+    // SmemCLayout [x_bit * block_m, w_bit * block_n]
+    using SmemCLayout =
+        decltype(make_layout(make_shape(Int<MainLoop_BLOCK_M>{}, Int<MainLoop_BLOCK_N>{}),
+                             make_stride(Int<MainLoop_BLOCK_N>{}, _1{})));
+
+    static constexpr size_t outputSmem_size = cosize(SmemCLayout{}) * sizeof(acc_type);
+    // total Smem usage max(A + B, C)
+    static constexpr size_t SmemSize = cute::max(inputSmemSize, outputSmem_size);
+
+    // --- Copy ---
+    using Copy = AqCuteCopy<type, acc_type, ThreadBlockShape, blockDims>;
+    using G2SCopyA = typename Copy::G2SCopyA;
+    using G2SCopyB = typename Copy::G2SCopyB;
+
+    using S2RCopyAtomA = typename Copy::S2RCopyAtomA;
+    using S2RCopyAtomB = typename Copy::S2RCopyAtomB;
+    using R2SCopyAtomC = typename Copy::R2SCopyAtomC;
+
+    using EpilogS2RCopy = typename Copy::EpilogS2RCopy;
+    using EpilogR2GCopy = typename Copy::EpilogR2GCopy;
+
+    // --- mainloop matmul [P * block_m, K] * [Q * block_n, K]T = [P * block_m, Q * block_n] ---
     __device__ __forceinline__ void mainLoop(const int M, const int N, const int K, const int *X,
                                              const int *W, int *shared_mem_workspace);
-
+    // --- epilogue reduce [P * block_m, Q * block_n] -> [block_m, block_n] ---
     __device__ __forceinline__ void epilogue(const int M, const int N, int *D,
                                              int *shared_mem_workspace, const half *C,
                                              bool bias = false);
